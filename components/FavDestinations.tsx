99abc548df6c1219ec1785f9aaf005f2cd462d55
Tussharr13 <tussharr24@gmail.com>
2025-06-29 04:01:01 +0530

    fav destinations

--- components/FavDestinations.tsx
+++ components/FavDestinations.tsx
@@ -7,10 +7,10 @@ import Image from "next/image"
 export default function FavDestinations() {
 
 const categories = [
-    { name: "Nainital", destination:"Nainital", src: "https://images.wanderon.in/blogs/new/2024/06/places-to-visit-in-nainital.jpeg" },
+    { name: "Ladakh", destination:"Leh, ladakh", src: "https://imgs.search.brave.com/fuuqt8gGGj-CTBGENYyjxvhTJ2Gthzfh3cyYP8maWOM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy8y/LzJmL0xlaXN1cmVf/TGVoX0xhZGFraC5q/cGc" },
     { name: "Manali", destination:"Manali, Himachal Pradesh",  src: "https://images.unsplash.com/photo-1712388430474-ace0c16051e2?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFuYWxpfGVufDB8fDB8fHww" },
-    { name: "Mussoorie", destination:"Mussoorie",  src: "https://s3.india.com/wp-content/uploads/2024/05/Feature-Image_-Mussoorie-5.jpg?impolicy=Medium_Widthonly&w=350&h=263" },
-    { name: "Rishikesh", destination:"Rishikesh",  src: "https://captureatrip-cms-storage.s3.ap-south-1.amazonaws.com/Rishikesh_3c33f1475f.webp" },
+    { name: "Hampta Pass", destination:"Hampta Pass",  src: "https://imgs.search.brave.com/wAUdxlha9RFkMBSFP7AY3bbZysu0z2-JRVLi_OkDOms/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9yZXMu/Y2xvdWRpbmFyeS5j/b20vZHlpZmZya3po/L2ltYWdlL3VwbG9h/ZC9jX2ZpbGwsZl9h/dXRvLGZsX3Byb2dy/ZXNzaXZlLnN0cmlw/X3Byb2ZpbGUsZ19j/ZW50ZXIsaF81MTgs/cV9hdXRvLHdfNjYw/L3YxNzAyNzA5MTk5/L2Jiai9kdnhxbTV0/cncweDRiZXZubnpq/ai5qcGc" },
+    { name: "Spiti Valley", destination:"Spiti Valley",  src: "https://www.tripsavvy.com/thmb/QO0P0dHsKwdycgi14QxO0hq2Jvk=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-519309190-7706d8f2e6f84dd0ad3b0ed1b164feff.jpg" },
   ]
 
 return(
