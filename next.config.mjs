0c30d827e52563ae023086379c538d062e0ec3b0
Tussharr13 <tussharr24@gmail.com>
2026-01-02 14:08:46 +0530

    another fix

--- next.config.mjs
+++ next.config.mjs
@@ -21,12 +21,7 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-  // Increase API route body size limit for file uploads
-  api: {
-    bodyParser: {
-      sizeLimit: '15mb',
-    },
-  },
+
 }
 
 mergeConfig(nextConfig, userConfig)
